eaecaf8853a7a98ef88571132bafe73eeac6b60f
Patjayden <patjayden11@gmail.com>
2026-05-06 20:40:22 +1000

    One key of keyboard is animated now

--- Keyboard.cs
+++ Keyboard.cs
@@ -1,19 +1,37 @@
 using Godot;
 using System;
 
-public partial class Keyboard : Node
+public partial class Keyboard : Sprite2D
 {
 	private AnimatedSprite2D keyboardAnim;
 	
 	public override void _Ready()
 	{
-		keyboardAnim = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 
-		keyboardAnim.Play(default);
+		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+		animatedSprite2D.Animation = "default";
+
+		Show();
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+
+		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+		if (Input.IsActionPressed("one_key"))
+		{
+			
+
+			animatedSprite2D.Animation = "one";
+			
+		}
+		else
+		{
+			animatedSprite2D.Animation = "default";
+		}
 	}
 }
